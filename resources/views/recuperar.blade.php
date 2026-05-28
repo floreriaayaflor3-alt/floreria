0cf8f3d2ee157505354001658746d123d29a7a34
Malondra <22161248@itoaxaca.edu.mx>
2026-05-28 17:52:17 -0600

    fix de corre

--- resources/views/recuperar.blade.php
+++ resources/views/recuperar.blade.php
@@ -33,7 +33,7 @@ body{
         <div class="alert alert-danger">{{ session('error') }}</div>
     @endif
 
-    <form method="POST" action="/recuperar">
+    <form method="post" action="/recuperar">
         @csrf
         <input type="email" name="correo" class="form-control mb-3" placeholder="Tu correo electrónico" required>
         <button class="btn w-100 text-white" style="background:#7b2d5b;">Enviar link de recuperación</button>
